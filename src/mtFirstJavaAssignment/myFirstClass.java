41f2a5eeda47ec56c83b1e19c0665fee48c2da74
syed7212 <syedfakhar012@gmail.com>
2020-01-29 20:11:53 -0500

    first commit

---
+++ src/mtFirstJavaAssignment/myFirstClass.java
@@ -0,0 +1,139 @@
+package mtFirstJavaAssignment;
+
+public class myFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		
+
+//	String housePrice = "$653,000";
+//	String address = "12024 valcour Island Way";
+//	String techSchool = "TechCircle";
+//	
+//	System.out.println(techSchool + " is an IT Bootcamp. And the Address is\n"+ 
+//	address + "." + " The medium price of the houses in the area is " + housePrice + ".");
+		
+		
+	////////////////////
+		//object data types
+			
+//			Integer objectNumber = new Integer(6);
+//			Integer objectSecNumber = new Integer(6);
+//			
+//			boolean b = (objectNumber.equals(objectSecNumber));
+//			System.out.println(b);
+			
+		///////////////
+			
+			//primitive data types
+			
+//			int firstNumber = 15;
+//			int secondNumber = 15;
+//			
+//			boolean a = (firstNumber==secondNumber);
+//			System.out.println(a);
+//			
+//			
+//			int x = 5;
+//			x *= 15;
+//			System.out.println(x);
+			
+
+//			int x = 5;
+//			
+//			x--;
+//			System.out.println(x);
+//x++;
+//System.out.println(x);
+		
+//         String firstName = "Fakhar";
+//         String secName = "Naqvi";
+//         
+//         System.out.println("My name is " + firstName + " " + secName + "!");
+//
+//
+//        String myHouse = "Fairfax";
+//        String housePrice = "$650000";
+//        
+//        System.out.println("I live in " + myHouse + " " + "and the price is" + " " + housePrice + "!");
+//
+//
+//
+//
+//         int myInt = 10;
+//         double myDouble = myInt;
+         
+//         System.out.println(myInt ==myDouble);
+    
+//         double myDouble = 9.43;
+//         int myInt = (Int) myDouble;
+//          System.out.println(myDouble);
+
+
+
+
+
+//         int sum1 = 150 + 150;
+//       int sum2 = sum1 + 200 + 200;
+//       int sum3 = sum1 + sum1 + 250;
+//       
+//      System.out.println(sum1);
+//      System.out.println(sum2);
+//       System.out.println(sum3);
+//
+//       String txt = "hello";
+//       System.out.println(txt.toUpperCase());
+       
+       
+       
+     String firstName = "John";
+     String lastName = "Doe";
+       
+        
+		System.out.println(firstName + lastName );
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+     
+
+
+
+
+
+
+
+
+
+		
+		
+		
+		
+
+	}
+
+}
